ade9abda8260141a65d95c0d142d7bcda17f7760
julien <98946443+julien44830@users.noreply.github.com>
2022-07-08 11:24:44 +0200

    on a avancer le html

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("bonjour");
+function newList (titre, tache){
+  const newTache = document.createElement("tr");
+  
+  newTache.innerHTML = `
+  <tr>
+    <th>titre de la tache</td>
+    <th>déscription de la tache</th>
+  </tr>
+  <br>
+  <tr>
+    <td class="btnSup"><input type="checkbox" id="coche"></th>
+    <td class="title">${titre}</td>
+    <td class="descript">${tache}</td>
+    <td class="btnSup"><button id="btn2">supprimer</button></td>
+  </tr>`;
+
+  document.querySelector("table").appendChild(newTache);
+}
+
+newList ('code todoListe' + 'continuer à coder');
