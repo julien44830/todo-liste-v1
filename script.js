504522981f7ca168992d4c0298976c4905f573d9
julien <deniaudjulien0@gmail.com>
2022-07-08 21:20:25 +0200

    sdvf

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-function newList (titre, tache){
-  const newTache = document.createElement("tr");
+
+const btn = document.getElementById('btn1');
+
+
+function newList (title, tache){
+  title = document.getElementById('title');
+  tache = document.getElementById('tache');
   
+  const newTache = document.createElement('tr');
+
   newTache.innerHTML = `
-  <tr>
-    <th>titre de la tache</td>
-    <th>déscription de la tache</th>
-  </tr>
-  <br>
-  <tr>
-    <td class="btnSup"><input type="checkbox" id="coche"></th>
-    <td class="title">${titre}</td>
-    <td class="descript">${tache}</td>
+  <tr id="newList" value="1">
+    <td class="btnSup"><input type="checkbox" name="" id="coche"></td>
+    <td class="title">${title.value}</td>
+    <td class="descript">${tache.value}</td>
     <td class="btnSup"><button id="btn2">supprimer</button></td>
   </tr>`;
-
+  
   document.querySelector("table").appendChild(newTache);
+  console.log(title.value);
 }
 
-newList ('code todoListe' + 'continuer à coder');
+
+btn.addEventListener("click", newList);
+
+
+
+
