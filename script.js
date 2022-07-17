79b3ce0e440ebe17f4f69cdbce0db455dd13422c
julien <deniaudjulien0@gmail.com>
2022-07-17 16:58:46 +0200

    ajout du code de confirmation à finir

--- script.js
+++ script.js
@@ -4,25 +4,30 @@ const contenu = document.getElementsByName('tache');
 
 
 function newList (){
-  title = document.getElementById('title');
-  tache = document.getElementById('tache');
+  const title = document.getElementById('title');
+  const tache = document.getElementById('tache');
   const newTache = document.createElement('tr');
   const table = "tableau";
   let checkbox = document.getElementsByClassName('btnSup');
   console.log(checkbox.checked);
+  const titleLong = title.lenght;
 
-newTache.innerHTML = `
-<tr class="" name="${table}"id="" name="nouvelleLigne" value="">
-  <td class="btnSup"><input type="checkbox" name="" onclick="(functFini)" id="coche" checked></td>
-  <td name="" class="title">${title.value}</td>
-  <td name="tache" class="descript">${tache.value}</td>
-  <td class="btnSup"><button id="btn2" type="reset">supprimer</button></td>
-</tr>
-`;
-
-  document.querySelector("table").appendChild(newTache);
-  console.log('titre ' + title.value);
-  console.log('tache ' + tache.value);
+  if (titleLong <= 0){
+    newTache.innerHTML = `
+    <tr class="" name="${table}"id="" name="nouvelleLigne" value="">
+      <td class="btnSup"><input type="checkbox" name="" onclick="(functFini)" id="coche" ></td>
+      <td name="" class="title">${title.value}</td>
+      <td name="tache" class="descript">${tache.value}</td>
+      <td class="btnSup"><button id="btn2" type="reset">supprimer</button></td>
+    </tr>
+    `;
+    
+      document.querySelector("table").appendChild(newTache);
+      console.log('titre ' + title.value);
+      console.log('tache ' + tache.value);
+  }else{
+    alert("veuiller rentrer une nouvelle tache");
+  }
 }
 
 function functFini(){
