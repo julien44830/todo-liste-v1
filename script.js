94f417a62fe7048fad868de9b294a864926c14ed
julien <deniaudjulien0@gmail.com>
2022-07-17 23:42:49 +0200

    ajout de la validation de formulaire en JS

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
 
 const btn = document.getElementById('btn1');
-const contenu = document.getElementsByName('tache');
-
 
 function newList (){
   const title = document.getElementById('title');
   const tache = document.getElementById('tache');
   const newTache = document.createElement('tr');
   const table = "tableau";
-  let checkbox = document.getElementsByClassName('btnSup');
-  console.log(checkbox.checked);
-  const titleLong = title.lenght;
-
-  if (titleLong <= 0){
+  const titleValue = title.value;
+  const titleLength = titleValue.length;
+  const tacheValue = tache.value;
+  const tacheLength = tacheValue.length;
+  console.log(titleValue);
+  console.log(titleLength);
+  console.log(checkbox);
+  console.log('titre ' + title.value);
+  console.log('tache ' + tache.value);
+  const i = 0;
+  
+  if (tacheLength || titleLength > i){
     newTache.innerHTML = `
     <tr class="" name="${table}"id="" name="nouvelleLigne" value="">
-      <td class="btnSup"><input type="checkbox" name="" onclick="(functFini)" id="coche" ></td>
-      <td name="" class="title">${title.value}</td>
+    <td class="btnSup"><input type="checkbox" name="" onclick="(functFini)" id="coche" checked ></td>
+    <td name="" class="title">${titleValue}</td>
       <td name="tache" class="descript">${tache.value}</td>
       <td class="btnSup"><button id="btn2" type="reset">supprimer</button></td>
     </tr>
     `;
     
-      document.querySelector("table").appendChild(newTache);
-      console.log('titre ' + title.value);
-      console.log('tache ' + tache.value);
+    document.querySelector("table").appendChild(newTache);
   }else{
-    alert("veuiller rentrer une nouvelle tache");
+      alert("veuiller rentrer une nouvelle tache");
+    }
   }
-}
 
-function functFini(){
-  if (checked == false){
-    contenu.ClassList.add('fini');
-  }else{
+  function functFini(){
+    if (checked == true){
+      contenu.ClassList.add('fini');
+    }else{
     contenu.classList.remove('fini');
   }
 }
 
 btn.addEventListener("click", newList);
-// checkbox.addEventListener("click", functFini);
-console.log("tr");
-console.log("h1");
 
-
-
-
+const checkbox = document.getElementById('coche');
+checkbox.addEventListener("change", functFini());
